2295a6e61a23114e0309c970c1305d92f1c3f85a
Nathan Reline <nathanielreline@gmail.com>
2020-11-22 18:43:28 -0700

    Convert availability helper to kotlin

--- src/test/java/com/github/pires/obd/utils/CommandAvailabilityHelperTest.java
+++ src/test/java/com/github/pires/obd/utils/CommandAvailabilityHelperTest.java
@@ -22,7 +22,7 @@ public class CommandAvailabilityHelperTest {
     public void testDigestAvailabilityString() throws Exception {
         int[] expected = new int[]{Integer.parseInt("10111110", 2), Integer.parseInt("00011111", 2),
                 Integer.parseInt("10101000", 2), Integer.parseInt("00010011", 2)};
-        int[] result = CommandAvailabilityHelper.digestAvailabilityString("BE1FA813");
+        int[] result = CommandAvailabilityHelper.INSTANCE.digestAvailabilityString("BE1FA813");
         Assert.assertEquals(expected, result);
 
         //Now with 16 characters
@@ -31,30 +31,30 @@ public class CommandAvailabilityHelperTest {
                 Integer.parseInt("10111110", 2), Integer.parseInt("00011111", 2),
                 Integer.parseInt("10101000", 2), Integer.parseInt("00010011", 2)};
 
-        result = CommandAvailabilityHelper.digestAvailabilityString("BE1FA813BE1FA813");
+        result = CommandAvailabilityHelper.INSTANCE.digestAvailabilityString("BE1FA813BE1FA813");
         Assert.assertEquals(expected, result);
     }
 
     @Test
     public void testIsAvailable() throws Exception {
-        Assert.assertEquals(CommandAvailabilityHelper.isAvailable("02", "BE1FA813"), false);
-        Assert.assertEquals(CommandAvailabilityHelper.isAvailable("07", "BE1FA813"), true);
-        Assert.assertEquals(CommandAvailabilityHelper.isAvailable(new ThrottlePositionCommand().getCommandPID() /*11*/, "BE1FA813"), true);
-        Assert.assertEquals(CommandAvailabilityHelper.isAvailable("1A", "BE1FA813"), false);
-        Assert.assertEquals(CommandAvailabilityHelper.isAvailable("1D", "BE1FA813"), false);
-        Assert.assertEquals(CommandAvailabilityHelper.isAvailable("1F", "BE1FA813"), true);
-        Assert.assertEquals(CommandAvailabilityHelper.isAvailable("22", "BE1FA813BE1FA813"), false);
-        Assert.assertEquals(CommandAvailabilityHelper.isAvailable("27", "BE1FA813BE1FA813"), true);
-        Assert.assertEquals(CommandAvailabilityHelper.isAvailable("3A", "BE1FA813BE1FA813"), false);
-        Assert.assertEquals(CommandAvailabilityHelper.isAvailable("3D", "BE1FA813BE1FA813"), false);
-        Assert.assertEquals(CommandAvailabilityHelper.isAvailable("3F", "BE1FA813BE1FA813"), true);
+        Assert.assertEquals(CommandAvailabilityHelper.INSTANCE.isAvailable("02", "BE1FA813"), false);
+        Assert.assertEquals(CommandAvailabilityHelper.INSTANCE.isAvailable("07", "BE1FA813"), true);
+        Assert.assertEquals(CommandAvailabilityHelper.INSTANCE.isAvailable(new ThrottlePositionCommand().getCommandPID() /*11*/, "BE1FA813"), true);
+        Assert.assertEquals(CommandAvailabilityHelper.INSTANCE.isAvailable("1A", "BE1FA813"), false);
+        Assert.assertEquals(CommandAvailabilityHelper.INSTANCE.isAvailable("1D", "BE1FA813"), false);
+        Assert.assertEquals(CommandAvailabilityHelper.INSTANCE.isAvailable("1F", "BE1FA813"), true);
+        Assert.assertEquals(CommandAvailabilityHelper.INSTANCE.isAvailable("22", "BE1FA813BE1FA813"), false);
+        Assert.assertEquals(CommandAvailabilityHelper.INSTANCE.isAvailable("27", "BE1FA813BE1FA813"), true);
+        Assert.assertEquals(CommandAvailabilityHelper.INSTANCE.isAvailable("3A", "BE1FA813BE1FA813"), false);
+        Assert.assertEquals(CommandAvailabilityHelper.INSTANCE.isAvailable("3D", "BE1FA813BE1FA813"), false);
+        Assert.assertEquals(CommandAvailabilityHelper.INSTANCE.isAvailable("3F", "BE1FA813BE1FA813"), true);
     }
 
     @Test(expectedExceptions = IllegalArgumentException.class)
     public void testFail() throws Exception {
-        CommandAvailabilityHelper.digestAvailabilityString("AAA");
-        CommandAvailabilityHelper.digestAvailabilityString("AAAAAAAR");
-        CommandAvailabilityHelper.isAvailable("2F", "BE1FA813");
+        CommandAvailabilityHelper.INSTANCE.digestAvailabilityString("AAA");
+        CommandAvailabilityHelper.INSTANCE.digestAvailabilityString("AAAAAAAR");
+        CommandAvailabilityHelper.INSTANCE.isAvailable("2F", "BE1FA813");
     }
 
 }
